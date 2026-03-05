c63831cd44abb460a53d649b45ca5b7e24e9407b
Palak294 <patelp46@myumanitoba.ca>
2026-03-04 22:54:28 -0600

    Added Flex theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,6 +1,6 @@
 AUTHOR = 'PALAK PATEL'
 SITENAME = 'Assignment website'
-SITEURL = ''
+SITEURL = 'https://palak294.github.io/Resume'
 PATH = "content"
 
 TIMEZONE = 'Canada/Central'
@@ -31,7 +31,7 @@ SOCIAL = (
 DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
-# RELATIVE_URLS = True
+# RELATIVE_URLS = False
 
 OUTPUT_PATH = 'docs/'
 THEME = 'themes/Flex'
